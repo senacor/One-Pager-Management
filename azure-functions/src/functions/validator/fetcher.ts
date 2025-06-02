761580cba07134bd9d958ba54bbc683e85a20595
Daniel Heinrich <dannynullzwo@gmail.com>
2025-06-02 10:18:08 +0200

    retry downloads

--- azure-functions/src/functions/validator/fetcher.ts
+++ azure-functions/src/functions/validator/fetcher.ts
@@ -1,5 +1,6 @@
 import { readFile } from "fs/promises";
 import { Logger } from "./DomainTypes";
+import { HardenedFetch } from "hardened-fetch";
 
 export async function fetchOnePagerContent(log: Logger, onePager: { fileLocation: URL }): Promise<Buffer> {
     if (onePager.fileLocation.protocol === 'file:') {
@@ -9,11 +10,19 @@ export async function fetchOnePagerContent(log: Logger, onePager: { fileLocation
         log.log(`Reading file from path: ${fsPath}`);
         return await readFile(fsPath);
     } else {
+        const client = new HardenedFetch({
+            // Retry options
+            maxRetries: 3,
+            doNotRetry: [400, 401, 403, 404, 422, 451],
+            // Rate limit options
+            rateLimitHeader: 'retry-after',
+            resetFormat: 'seconds',
+        });
         // HTTP(S) fetch
         const url = onePager.fileLocation.toString();
         log.log(`Fetching file from URL: ${url}`);
-        const response = await fetch(url);
-        if(response.status !== 200) {
+        const response = await client.fetch(url);
+        if (response.status !== 200) {
             throw new Error(`failed to fetch ${url}, returned with status ${response.status}: ${await response.text()}`)
         }
         return Buffer.from(await response.arrayBuffer());
