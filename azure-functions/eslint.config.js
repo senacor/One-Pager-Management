f1076d48c00454a9667002e2dfaa05cdcd1cebbf
Konschin <artjom.konschin@senacor.com>
2025-06-03 14:52:22 +0200

    .

--- azure-functions/eslint.config.js
+++ azure-functions/eslint.config.js
@@ -1,11 +1,9 @@
-// eslint.config.js
-import { defineConfig } from "eslint/config";
+// @ts-check
 
-export default defineConfig([
-	{
-		rules: {
-			semi: "error",
-			"prefer-const": "error",
-		},
-	},
-]);
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
