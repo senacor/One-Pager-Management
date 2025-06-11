cdabee196cb4b30a7d9b6a7b8847bec2d5796810
Daniel Heinrich <dannynullzwo@gmail.com>
2025-06-11 09:31:27 +0200

    minor

--- azure-functions/src/functions/FileChangeHttpTrigger.ts
+++ azure-functions/src/functions/FileChangeHttpTrigger.ts
@@ -26,21 +26,19 @@ export async function FileChangeHttpTrigger(
 ): Promise<HttpResponseInit> {
     context.log(`--------- Trigger FileChangeHttpTrigger ---------`);
     try {
-        context.log(
-            `(FileChangeHttpTrigger.ts: FileChangeHttpTrigger) HTTP function processed request for url "${request.url}"`,
-        );
+        context.log(`HTTP function processed request for url "${request.url}"`);
 
         // Extract employee ID from the request parameters
         const id = request.params.employeeid;
         if (!isEmployeeId(id)) {
-            context.log(`(FileChangeHttpTrigger.ts: FileChangeHttpTrigger) Invalid employee id: "${id}"!`);
+            context.log(`Invalid employee id: "${id}"!`);
             return { status: 400, body: `Invalid request! "${id}" is no valid employee id.` };
         }
 
         // Load the list of employees from the configuration
         const employees = await (await loadConfigFromEnv(context)).employees();
         if (!(await employees.getAllEmployees()).includes(id)) {
-            context.log(`(FileChangeHttpTrigger.ts: FileChangeHttpTrigger) Employee not found: "${id}"!`);
+            context.log(`Employee not found: "${id}"!`);
             return { status: 404, body: `Employee not found: "${id}"` };
         }
 
@@ -48,11 +46,11 @@ export async function FileChangeHttpTrigger(
         const item: QueueItem = { employeeId: id };
         context.extraOutputs.set(queueOutput, item);
 
-        context.log(`(FileChangeHttpTrigger.ts: FileChangeHttpTrigger) Queue item created for employee id: "${id}"!`);
+        context.log(`Queue item created for employee id: "${id}"!`);
         return { body: `Received change notification for: "${id}"` };
     } catch (error) {
         context.error(
-            `(FileChangeHttpTrigger.ts: FileChangeHttpTrigger) Error processing request: "${printError(error)}"!`,
+            `Error processing request: "${printError(error)}"!`,
         );
         return { status: 500, body: `Internal server error` };
     } finally {
